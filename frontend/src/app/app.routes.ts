ae14e19181f6de0be6820327c0c7d813ce00e0c7
killian.hedou@gmail.com <killian.hedou@gmail.com>
2024-11-05 16:34:49 -0500

    Added tracking route, and component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
+import { LiveTrackingComponent } from './live-tracking/live-tracking.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'live-tracking', component: LiveTrackingComponent },
 ];
---
+++ frontend/src/app/live-tracking/live-tracking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-tracking',
+  standalone: true,
+  imports: [],
+  templateUrl: './live-tracking.component.html',
+  styleUrl: './live-tracking.component.scss'
+})
+export class LiveTrackingComponent {
+
+}
