1787a0ffd555c2715733541b121113f52967641d
Vito <vito.rizzuto190@gmail.com>
2024-11-10 22:58:08 -0500

    adding name if not signed in

--- frontend/src/app/delivery-request/delivery-request.component.ts
+++ frontend/src/app/delivery-request/delivery-request.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   standalone: true,
   templateUrl: './delivery-request.component.html',
   styleUrls: ['./delivery-request.component.scss'],
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule], // Ensure ReactiveFormsModule is imported here
 })
 export class DeliveryRequestComponent implements OnInit {
   deliveryForm: FormGroup;
@@ -19,18 +19,45 @@ export class DeliveryRequestComponent implements OnInit {
     this.deliveryForm = this.fb.group({
       pickup: ['', [Validators.required]],
       dropOff: ['', [Validators.required]],
+      signedIn: [false, [Validators.required]],
+      name: [''],
+      email: ['', [Validators.email]],
       specialInstructions: [''],
       deliveryType: ['standard', [Validators.required]]
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Set validators for name and email if the user is not signed in
+    this.deliveryForm.get('signedIn')?.valueChanges.subscribe((signedIn) => {
+      if (!signedIn) {
+        this.deliveryForm.get('name')?.setValidators([Validators.required]);
+        this.deliveryForm.get('email')?.setValidators([Validators.required, Validators.email]);
+      } else {
+        this.deliveryForm.get('name')?.clearValidators();
+        this.deliveryForm.get('email')?.clearValidators();
+      }
+      this.deliveryForm.get('name')?.updateValueAndValidity();
+      this.deliveryForm.get('email')?.updateValueAndValidity();
+    });
+  }
+
+  onSignedInChange() {
+    const signedIn = this.deliveryForm.get('signedIn')?.value;
+    if (!signedIn) {
+      this.deliveryForm.get('name')?.setValidators([Validators.required]);
+      this.deliveryForm.get('email')?.setValidators([Validators.required, Validators.email]);
+    } else {
+      this.deliveryForm.get('name')?.clearValidators();
+      this.deliveryForm.get('email')?.clearValidators();
+    }
+    this.deliveryForm.get('name')?.updateValueAndValidity();
+    this.deliveryForm.get('email')?.updateValueAndValidity();
+  }
 
   onSubmit() {
     if (this.deliveryForm.valid) {
       console.log('Delivery request submitted:', this.deliveryForm.value);
-
-      // Show the confirmation message
       this.showConfirmation = true;
     } else {
       this.markFormGroupTouched(this.deliveryForm);
@@ -38,12 +65,7 @@ export class DeliveryRequestComponent implements OnInit {
   }
 
   navigateToQuote() {
-    const origin = this.deliveryForm.get('pickup')?.value;
-    const destination = this.deliveryForm.get('dropOff')?.value;
-  
-    this.router.navigate(['/quote-request'], {
-      state: { origin, destination }
-    });
+    this.router.navigate(['/quote-request']);
   }
 
   markFormGroupTouched(formGroup: FormGroup): void {
