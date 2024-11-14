d2db0e16b1a1ce586927a6671a3b7fa045103c6e
Matthew <matthewpompeo22@gmail.com>
2024-11-14 13:33:56 -0500

    ShippingLabel Styling

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
 import { PaymentComponent } from '../app/payment/payment.component';
 import {QuoteRequestComponent} from "./quote/quote-request.component";
+import {ShippingLabelComponent} from "./shippinglabel/shippinglabel.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'payment', component: PaymentComponent },
-    { path: 'quote-request', component: QuoteRequestComponent}
+    { path: 'quote-request', component: QuoteRequestComponent},
+    {path: 'shippinglabel', component: ShippingLabelComponent},
 ];
